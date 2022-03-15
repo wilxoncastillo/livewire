6c33e7a00ccfd9476cc35c7c979aa26564e242f0
Wilson Castillo <wilxoncastillo@gmail.com>
2022-03-15 10:36:35 -0400

    Acciones en Livewire

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,7 +1,7 @@
-<section class="antialiased bg-gray-100 text-gray-600 h-screen px-4">
+<section class="antialiased bg-gray-100 text-gray-600 h-min px-4">
     <div class="flex flex-col justify-center h-full">
         <!-- Table -->
-        <div class="w-full max-w-2xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200">
+        <div class="w-full mx-auto bg-white shadow-lg rounded-sm border border-gray-200">
             <div class="p-3">
                 <div class="overflow-x-auto">
                     {{ $slot}}
@@ -10,3 +10,4 @@
         </div>
     </div>
 </section>
+
--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -1,4 +1,4 @@
-<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
@@ -10,19 +10,55 @@
             <x-jet-input type="text" wire:model='search' class="w-full" placeholder="Escriba lo que quiere buscar"/>
         </div>
         @if($posts->count())
-            <table class="table-auto w-full">
+            <table class="table">
                 <thead class="text-xs font-semibold uppercase text-gray-400 bg-gray-50">
                     <tr>
-                        <th class="p-2 whitespace-nowrap">
-                            <div class="font-semibold text-left">Id</div>
+                        <th class="cursor-pointer p-2 w-20" wire:click="order('id')">
+                            <div class="font-semibold text-left">
+                                @if($sort == 'id')
+                                    @if($direction == 'asc')
+                                        <i class="fa fa-sort-alpha-up-alt"></i>
+                                    @else
+                                        <i class="fa fa-sort-alpha-down-alt"></i>
+                                    @endif
+                                @else
+                                    <i class="fa fa-sort"></i>
+                                @endif
+
+                                Id
+                            </div>
                         </th>
-                        <th class="p-2 whitespace-nowrap">
-                            <div class="font-semibold text-left">Title</div>
+                        <th class="cursor-pointer p-2" wire:click="order('title')">
+                            <div class="font-semibold text-left">
+                                @if($sort == 'title')
+                                    @if($direction == 'asc')
+                                        <i class="fa fa-sort-alpha-up-alt"></i>
+                                    @else
+                                        <i class="fa fa-sort-alpha-down-alt"></i>
+                                    @endif
+                                @else
+                                    <i class="fa fa-sort"></i>
+                                @endif
+
+                                Title
+                            </div>
                         </th>
-                        <th class="p-2 whitespace-nowrap">
-                            <div class="font-semibold text-left">Content</div>
+                        <th class="cursor-pointer p-2 whitespace-wrap" wire:click="order('content')">
+                            <div class="font-semibold text-left">
+                                 @if($sort == 'content')
+                                    @if($direction == 'asc')
+                                        <i class="fa fa-sort-alpha-up-alt"></i>
+                                    @else
+                                        <i class="fa fa-sort-alpha-down-alt"></i>
+                                    @endif
+                                @else
+                                    <i class="fa fa-sort"></i>
+                                @endif
+
+                                Content
+                            </div>
                         </th>
-                        <th class="p-2 whitespace-nowrap">
+                        <th class="p-2">
                             <div class="font-semibold text-center">Edit</div>
                         </th>
                     </tr>
@@ -30,16 +66,16 @@
                 <tbody class="text-sm divide-y divide-gray-100">
                     @foreach($posts as $post)
                     <tr>
-                        <td class="p-2 whitespace-nowrap">
+                        <td class="p-2">
                             <div class="text-left">{{ $post->id }}</div>
                         </td>
-                        <td class="p-2 whitespace-nowrap">
+                        <td class="p-2">
                             <div class="text-left">{{ $post->title }}</div>
                         </td>
-                        <td class="p-2 whitespace-nowrap">
+                        <td class="p-2">
                             <div class="text-left">{{ $post->content }}</div>
                         </td>
-                        <td class="p-2 whitespace-nowrap">
+                        <td class="p-2">
                             <div class="text-left">Edit</div>
                         </td>
                     </tr>
